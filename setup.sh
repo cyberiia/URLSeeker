aef6ea4d3759cc2060e106d50ded11aab0046dfb
Cyberia <87562989+cyberiia@users.noreply.github.com>
2023-02-27 04:59:22 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ PATHFILE=/etc/bash.bashrc
 
 if [ -f $PATHFILE ]; then
         chmod a+x dirhunter && echo "export PATH=$PATH:$PWD" >> $PATHFILE
-        echo -e "\x1B[1;36mSuccessful installation.\x1B[0m"
+        echo -e "\x1B[32m[+]\x1B[0m Successful installation."        
         sleep 2 && clear
 else
-        echo -e "\x1B[1;31mConfiguration file $DIRPATH not found.\x1B[0m"
+        echo -e "\x1B[31m[-]\x1B[0mConfiguration file $PATHFILE not found.\x1B[0m\n\x1B[31m[-]\x1B[0m Exiting..."
 fi
 
 
